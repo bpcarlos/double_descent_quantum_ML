9d6603af41a2322f0861fbcc845a7f78c94cf7b7
Carlos Bravo-Prieto <charl_ap@hotmail.es>
2024-12-18 12:14:22 +0100

    Add files via upload

---
+++ projected_kernel/Synthetic/qkerneldd.py
@@ -0,0 +1,153 @@
+#%%
+import pennylane as qml
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+
+#%% parameters -------------------------------------------------------------------------------
+n_test = 100
+n_train_max  = 2500  # this has to be larger than 4**n_qubits
+n_qubits = 2
+reps = 5
+
+#%% different functions to generate data ---------------------------------------------------------------
+
+def generate_evenly_spaced_samples(low, high, n_test, n_qubits):
+    num_points_per_dim = int(np.ceil(n_test ** (1 / n_qubits)))
+    linspaces = [np.linspace(start=low, stop=high, num=num_points_per_dim) for _ in range(n_qubits)]
+    grid = np.meshgrid(*linspaces, indexing='ij')
+    x_test_full = np.stack(grid, axis=-1).reshape(-1, n_qubits)
+    if x_test_full.shape[0] > n_test:
+        indices = np.linspace(0, x_test_full.shape[0] - 1, n_test, dtype=int)
+        x_test = x_test_full[indices]
+    else:
+        x_test = x_test_full
+    return x_test.astype(np.float64)
+
+def compute_y_from_x_multidim_linear(X):
+    weights = np.arange(1, n_qubits + 1, dtype=np.float64)
+    y = np.dot(X, weights)
+    return y
+
+#%% define quantum kernel functions -------------------------------------------------------
+
+def quantum_feature_map(x):
+    reps = n_qubits
+    for r in range(reps):
+        for i in range(len(x)):
+            qml.RX(x[i], wires=i % n_qubits)
+        for n in range(n_qubits - 1):
+            qml.CNOT(wires=[n, n + 1])
+        for i in range(len(x)):
+            qml.RZ(x[i], wires=i % n_qubits)
+
+dev_kernel = qml.device("default.qubit", wires=n_qubits)
+
+@qml.qnode(dev_kernel)
+def kernel_state(x):
+    quantum_feature_map(x)
+    return [qml.density_matrix(wires=i) for i in range(n_qubits)]
+
+def kernel_gauss(rho_x1, rho_x2):
+    delta = sum(qml.math.norm(rho1 - rho2) ** 2 for rho1, rho2 in zip(rho_x1, rho_x2))
+    return delta / n_qubits
+
+def kernel(x1, x2):
+    rho_x1 = kernel_state(x1)
+    rho_x2 = kernel_state(x2)
+    return kernel_gauss(rho_x1, rho_x2)
+
+def kernel_matrix(A, B):
+    return np.array([[kernel(a, b) for b in B] for a in A])
+
+#%% loop over different number of train data -------------------------------------------------
+
+mse_arr = [[] for r in range(reps)]
+mse_arr_train = [[] for r in range(reps)]
+eig_vals_arr = [[] for r in range(reps)]
+cond_no_arr = [[] for r in range(reps)]
+rank_arr = [[] for r in range(reps)]
+params = [[] for r in range(reps)]
+
+low, high = -1.0, 1.0
+x_test = generate_evenly_spaced_samples(low, high, n_test, n_qubits)
+y_test = compute_y_from_x_multidim_linear(x_test)
+scaler2 = MinMaxScaler(feature_range=(-np.pi/2, np.pi/2))
+x_test = scaler2.fit_transform(x_test)
+
+for r in range(reps):
+    print("rep:", r+1, "/", reps)
+    x_train_max = np.random.uniform(low=low, high=high, size=(n_train_max, n_qubits))
+    y_train_max = compute_y_from_x_multidim_linear(x_train_max)
+    x_train_max = scaler2.transform(x_train_max)
+
+    if n_qubits == 1:
+        limit_low = 1
+        limit_up = 10 
+        step = 1
+    if n_qubits == 2:
+        limit_low = 3  
+        limit_up = 14 
+        step = 1
+    elif n_qubits == 3:
+        limit_low = 4  
+        limit_up = 19
+        step = 1
+    elif n_qubits == 4:
+        limit_low = 6  
+        limit_up = 23
+        step = 1
+    elif n_qubits == 5:
+        limit_low = 8
+        limit_up = 27
+        step = 1
+
+    for n_train in range(limit_low, limit_up, step):    
+        x_train = x_train_max[:n_train]
+        y_train = y_train_max[:n_train]
+        print(r+1, ": train samples: ", x_train.shape[0], "/", limit_up-1)
+     
+        K_train = kernel_matrix(x_train, x_train).astype(np.float64)
+        K_test = kernel_matrix(x_test, x_train).astype(np.float64)
+
+        kernel_ridge_model = KernelRidge(kernel='precomputed', alpha=0.0)
+        kernel_ridge_model.fit(K_train, y_train)
+
+        predictions = kernel_ridge_model.predict(K_test)
+        train_predictions = kernel_ridge_model.predict(K_train)
+
+        params[r].append(kernel_ridge_model.dual_coef_)
+
+        K_eig_vals = np.linalg.eig(K_train)[0]
+        cond_no = np.linalg.cond(K_train)
+        rank = np.linalg.matrix_rank(K_train)
+
+        eig_vals_arr[r].append(K_eig_vals)
+        cond_no_arr[r].append(cond_no)
+        rank_arr[r].append(rank)
+        print("Rank of Kernel: ", rank)
+
+        mse = mean_squared_error(np.real(y_test), predictions)
+        mse_arr[r].append(mse)
+        print("Mean Squared Error Test:", mse)
+
+        train_mse = mean_squared_error(y_train, train_predictions)
+        mse_arr_train[r].append(train_mse)
+        print("Mean Squared Error Train:", train_mse)
+
+mse_arr = np.array(mse_arr)
+
+# %% Save results -----------------------------------------------------------------------
+mse_mean = np.mean(mse_arr, axis=0, dtype=np.float64)
+mse_std = np.std(mse_arr, axis=0) / (np.log(10))
+
+mse_mean_train = np.mean(mse_arr_train, axis=0)
+mse_std_train = np.std(mse_arr_train, axis=0)
+
+np.savetxt(f"mse_mean_nqubits_{n_qubits}_Synthetic", [mse_mean], newline='')
+np.savetxt(f"mse_std_nqubits_{n_qubits}_Synthetic", [mse_std], newline='')
+np.savetxt(f"mse_mean_train_nqubits_{n_qubits}_Synthetic", [mse_mean_train], newline='')
+np.savetxt(f"mse_std_train_nqubits_{n_qubits}_Synthetic", [mse_std_train], newline='')
+np.savetxt(f"arrange_nqubits_{n_qubits}_Synthetic", [np.arange(limit_low, limit_up, step)], newline='')
